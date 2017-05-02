da00a51f3c7d22177fcf254836de4d9c201aa7c5
hari krisha <hari.k.krishna4@gmail.com>
2017-05-02 14:37:52 -0500

    rootDir and exclude

--- TypeScript/dist/main.js
+++ TypeScript/dist/main.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("./two");
 var App = (function () {
     function App() {
+        console.log("I am main");
     }
     return App;
 }());
+new App();
