66798e3b428474beab6c170224ca758854445d2f
hari krisha <hari.k.krishna4@gmail.com>
2017-05-02 13:52:08 -0500

    on error no outuput

---
+++ TypeScript/dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./two");
+var App = (function () {
+    function App() {
+    }
+    return App;
+}());
--- TypeScript/dist/two.js
+++ TypeScript/dist/two.js
@@ -1,4 +1,3 @@
-"use strict";
 var Two = (function () {
     function Two() {
     }
