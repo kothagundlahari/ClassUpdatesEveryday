7b25214f1788d8785efdab997d0d4560c95996dc
hari krisha <hari.k.krishna4@gmail.com>
2017-05-04 12:59:17 -0500

    notworking

--- getting_started_with_angular2/angular2-fundamentals/src/app/app.component.ts
+++ getting_started_with_angular2/angular2-fundamentals/src/app/app.component.ts
@@ -1,20 +1,14 @@
 import { Component, Inject } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   template: `<div>
-
 <ul>
-  <li *ngFor="let message of mail.messages">
-    {{message}}
-  </li>
-</ul>
+  <app-simple-form  *ngFor="let message of mail.messages"
+    [message]="message" >
 
+  </app-simple-form>
+</ul>
 </div>`
 })
-export class AppComponent {
-
-  constructor(
-    @Inject('mail') private mail
-  ){}
-}
--- getting_started_with_angular2/angular2-fundamentals/src/app/simple-form/simple-form.component.ts
+++ getting_started_with_angular2/angular2-fundamentals/src/app/simple-form/simple-form.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-simple-form',
   template: `<div>
-
+{{message}}
 <input #myInput type="text">
-<button (mouseover)="onClick($event, myInput.value)">Click me!</button>
-
+<button (click)="onClick(myInput.value)">Click me!</button>
 </div>`,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
 
-  onClick(event, value){
-    console.log(event);
+  @Input() message;
+
+  onClick(value) {
+
     console.log(value);
   }
 
   constructor() { }
-
   ngOnInit() {
-  }
 
+  }
 }
