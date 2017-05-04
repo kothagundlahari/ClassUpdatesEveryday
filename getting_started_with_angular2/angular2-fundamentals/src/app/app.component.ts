bfc82f05f0011e78bb67e05e695c4b628a29eb1c
hari krisha <hari.k.krishna4@gmail.com>
2017-05-04 12:11:44 -0500

    injecting using provide and UseClass and UseValue

--- getting_started_with_angular2/angular2-fundamentals/src/app/app.component.ts
+++ getting_started_with_angular2/angular2-fundamentals/src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-import { MailService } from './mail.service'
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `<div>
 
 <app-simple-form></app-simple-form>
+
 {{mail.message}}
+<hr>
+{{api}}
+
 </div>`
 })
 export class AppComponent {
   title = `Let's get started!`;
-   constructor(private mail:MailService){}
+
+  constructor(
+    @Inject('mail') private mail,
+    @Inject('api') private api
+  ){}
 }
