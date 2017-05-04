1423d41347c52293c7237e16286560dabc2acbf3
hari krisha <hari.k.krishna4@gmail.com>
2017-05-04 11:43:16 -0500

    app is working using components

--- getting_started_with_angular2/angular2-fundamentals/src/app/app.component.ts
+++ getting_started_with_angular2/angular2-fundamentals/src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div>
+
+<app-simple-form></app-simple-form> 
+
+</div>`
 })
 export class AppComponent {
-  title = `this is the new text`;
+  title = `Let's get started!`;
 }
