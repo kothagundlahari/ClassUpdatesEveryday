8c09b17708ca7fa3923ba08e4bd157d6c1532844
hari krisha <hari.k.krishna4@gmail.com>
2017-05-11 15:32:38 -0500

    working with clock and date

--- reduxStyleApp/src/app/app.component.ts
+++ reduxStyleApp/src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
+
   template: `
-    <h1>{{clock | async}}</h1> `,
+    <h1>{{clock | async | date: 'yMMMMEEEdjms' }}</h1> `,
 
 })
 export class AppComponent {
-  clock = Observable.interval(1000);
+  clock = Observable
+    .interval(1000)
+    .map(() => new Date );
+
 
   constructor(){
     this.clock.subscribe(console.log.bind(console));
