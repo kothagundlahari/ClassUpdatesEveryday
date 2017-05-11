fdc7e0a4c3dbc51e84c759a55e38985e75922546
hari krisha <hari.k.krishna4@gmail.com>
2017-05-11 15:15:53 -0500

    working with the clock

--- reduxStyleApp/src/app/app.component.ts
+++ reduxStyleApp/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{clock | async}}</h1> `,
+
 })
 export class AppComponent {
-  title = 'app works!';
+  clock = Observable.interval(1000);
+
+  constructor(){
+    this.clock.subscribe(console.log.bind(console));
+  }
 }
