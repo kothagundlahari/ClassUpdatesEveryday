0ff5d120f6654a4f9384645666a82612eb04b27a
hari krisha <hari.k.krishna4@gmail.com>
2017-05-12 13:18:23 -0500

    added the rxjs project

--- reduxStyleApp/src/app/app.component.ts
+++ reduxStyleApp/src/app/app.component.ts
@@ -2,21 +2,28 @@ import { Component } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
+import {Subject} from 'rxjs/Subject';
+
+
 
 @Component({
   selector: 'app-root',
 
   template: `
+    <button (click)="click$.next()">Update</button>
     <h1>{{clock | async | date: 'yMMMMEEEdjms' }}</h1> `,
 
 })
 export class AppComponent {
-  clock = Observable
-    .interval(1000)
-    .map(() => new Date );
+
+  click$ = new Subject();
+  clock ;
 
 
-  constructor(){
-    this.clock.subscribe(console.log.bind(console));
-  }
+   constructor() {
+     this.clock = this.click$.map(() => {
+       return new Date();
+     });
+   }
+
 }
