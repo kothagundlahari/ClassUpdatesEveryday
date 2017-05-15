cd82c1cd3dd7ddb9631daaf8be02f47d8874e15f
hari krisha <hari.k.krishna4@gmail.com>
2017-05-15 13:06:45 -0500

    worked with merge

--- reduxApp/src/app/app.component.ts
+++ reduxApp/src/app/app.component.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
 import {Subject} from '../../../typeScript2/node_modules/rxjs/Subject';
+import 'rxjs/add/observable/merge';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +19,10 @@ export class AppComponent {
 
   clock;
   constructor() {
-    this.clock = this.click$.map(() => new Date());
+    this.clock = Observable.merge(
+      this.click$,
+      Observable.interval(5000)
+    ).map(() => new Date());
   }
 
 
