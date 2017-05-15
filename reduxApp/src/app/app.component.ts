1d0fb48fc0feebdc6b14a0186929608270ae91ce
hari krisha <hari.k.krishna4@gmail.com>
2017-05-15 12:02:13 -0500

    worked on the clock operator

--- reduxApp/src/app/app.component.ts
+++ reduxApp/src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   template: `
-  <h1>{{ clock | async}}</h1>`,
+  <h1>{{ clock | async | date: 'yMMMMEEEEdjms' }}</h1>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- clock = Observable.interval(1000);
+ clock = Observable
+   .interval(1000)
+   .map(() => new Date ());
 
- constructor(){
-   this.clock.subscribe(console.log.bind(console));
- };
+
 
 }
