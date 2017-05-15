57222c3a97c2247f853e59b079c32769ff1886a8
hari krisha <hari.k.krishna4@gmail.com>
2017-05-15 12:56:57 -0500

    working with subject

--- reduxApp/src/app/app.component.ts
+++ reduxApp/src/app/app.component.ts
@@ -1,18 +1,26 @@
+///<reference path="../../../typeScript2/node_modules/rxjs/add/operator/map.d.ts"/>
 import { Component } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
+import {Subject} from '../../../typeScript2/node_modules/rxjs/Subject';
 
 @Component({
   selector: 'app-root',
   template: `
+    <button (click)="click$.next()">Update </button>
   <h1>{{ clock | async | date: 'yMMMMEEEEdjms' }}</h1>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- clock = Observable
-   .interval(1000)
-   .map(() => new Date ());
+
+  click$ = new Subject();
+
+  clock;
+  constructor() {
+    this.clock = this.click$.map(() => new Date());
+  }
+
 
 
 
