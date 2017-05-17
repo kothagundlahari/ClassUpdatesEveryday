6a1268a836de4745d6a3c55c97e6d842a1fac045
hari krisha <hari.k.krishna4@gmail.com>
2017-05-17 11:45:57 -0500

    created the firstReactApp

--- reduxApp/src/app/app.component.ts
+++ reduxApp/src/app/app.component.ts
@@ -1,16 +1,21 @@
-///<reference path="../../../typeScript2/node_modules/rxjs/add/operator/map.d.ts"/>
-import { Component } from '@angular/core';
+///<reference path="../../../typeScript2/node_modules/rxjs/add/operator/mapTo.d.ts"/>
+
+import {Component} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
 import {Subject} from '../../../typeScript2/node_modules/rxjs/Subject';
 import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/mapTo';
+
 
 @Component({
   selector: 'app-root',
   template: `
-    <button (click)="click$.next()">Update </button>
-  <h1>{{ clock | async | date: 'yMMMMEEEEdjms' }}</h1>`,
+    <button (click)="click$.next()">Update</button>
+    <h1>{{ clock | async | date: 'yMMMMEEEEdjms' }}</h1>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
@@ -18,14 +23,27 @@ export class AppComponent {
   click$ = new Subject();
 
   clock;
+
   constructor() {
     this.clock = Observable.merge(
-      this.click$,
-      Observable.interval(5000)
-    ).map(() => new Date());
+      this.click$.mapTo('hour'),
+      Observable.interval(1000).mapTo('second')
+    )
+      .startWith(new Date())
+      .scan((acc, curr) => {
+        const date = new Date(acc.getTime());
+
+        if (curr === 'second') {
+          date.setSeconds(date.getSeconds() + 1);
+        }
+
+        if (curr === 'hour') {
+          date.setHours(date.getHours() + 1);
+        }
+
+        return date;
+      });
   }
-
-
 
 
 }
--- reduxApp/src/app/app.module.ts
+++ reduxApp/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+
+
+
 
 @NgModule({
   declarations: [
