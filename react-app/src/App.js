141e4ae3e4e4abb861eac879ccd716706100d8f8
hari krisha <hari.k.krishna4@gmail.com>
2017-06-05 11:58:55 -0500

    worked with Array Prop Types

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
-class App extends React.Component{
+class App extends React.Component {
     render(){
-       return (
-
-           <div>
-               <h1>Hello World, Hello Egg heads</h1>
-
-               <p>This is paragraph</p>
-           </div>
-
-       )
+        let txt = this.props.txt
+        return <h1>{txt}</h1>
     }
 }
 
-
 //const App = () => <h1>Hello, Kothagundla </h1>
+
+App.PropTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number
+}
+App.defaultProps = {
+    txt: "this is the default txt"
+}
 
 export  default  App
