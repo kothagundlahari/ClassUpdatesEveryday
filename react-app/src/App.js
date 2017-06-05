b29f62089f3760aeecd0f67e19725eddd22b54a0
hari krisha <hari.k.krishna4@gmail.com>
2017-06-05 11:22:19 -0500

    return with multiple nodes or elements

---
+++ react-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+       return (
+
+           <div>
+               <h1>Hello World, Hello Egg heads</h1>
+
+               <p>This is paragraph</p>
+           </div>
+
+       )
+    }
+}
+
+
+//const App = () => <h1>Hello, Kothagundla </h1>
+
+export  default  App
