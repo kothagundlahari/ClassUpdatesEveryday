949d26819da75f65cdcee6e985b98f2acac42012
hari krisha <hari.k.krishna4@gmail.com>
2017-06-06 11:30:32 -0500

    React Component Lifecycle Methods

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,37 +1,55 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
-    constructor(){
+    constructor() {
         super();
-        this.state = {a: '', b: ''}
+        this.state = {val: 0};
+        this.update = this.update.bind(this)
     }
-    update(){
-        this.setState({
-            a: this.a.refs.input.value,
-            b: this.refs.b.value
-        })
+
+    update() {
+        this.setState({val: this.state.val + 1})
     }
-    render(){
+
+    static componentWillMount() {
+        console.log('componentWillMount')
+    }
+
+    render() {
+        console.log('render');
+        return <button onClick={this.update}>{this.state.val}</button>
+    }
+
+    static componentDidMount() {
+        console.log('componentDidMount')
+    }
+
+    static componentWillUnmount(){
+        console.log('componentWillUnmount')
+    }
+
+
+}
+
+class Wrapper extends React.Component {
+    static mount(){
+        ReactDOM.render(<App />, document.getElementById('a'))
+    }
+    static unmount(){
+        ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+    }
+
+    render() {
         return (
             <div>
-                <Input
-                    ref={ component => this.a = component}
-                    update={this.update.bind(this)}
-                /> {this.state.a}
-                <hr />
-                <input
-                    ref="b"
-                    type="text"
-                    onChange={this.update.bind(this)}
-                /> {this.state.b}
+                <button onClick={Wrapper.mount.bind(this)}>Mount</button>
+                <button onClick={Wrapper.unmount.bind(this)}>UnMount</button>
+                <div id="a"></div>
             </div>
+
         )
     }
 }
 
-class Input extends React.Component {
-    render(){
-        return <div><input ref="input" type="text" onChange={this.props.update}/></div>
-    }
-}
-
-export default App
+export default Wrapper
